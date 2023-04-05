8cd7b88e3d53929f85f06fd9f7c1df732c2fff42
Saad bin abid <saad.abid@crowdbotics.com>
2023-04-05 22:25:41 +0500

    added reactjs app

---
+++ admin-frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Admin Home
+    </div>
+  );
+}
+
+export default App;
---
+++ customer-frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Customer Home
+    </div>
+  );
+}
+
+export default App;
